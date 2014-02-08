122bc86e3817bc2ad3e999d437e763184a9ba54d
Aimeast <lixd3389@gmail.com>
2014-02-08 13:23:53 +0800

    Add single job

---
+++ GitCandy/Schedules/SingleJob.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Composition;
+using System.Diagnostics.Contracts;
+
+namespace GitCandy.Schedules
+{
+    public class SingleJob : IJob
+    {
+        private Action _act;
+        private double _secondDue;
+
+        [ImportingConstructor]
+        public SingleJob()
+            : this(() => { }, 1.0)
+        { }
+
+        public SingleJob(Action action, double secondDue = 4.0)
+        {
+            Contract.Requires(action != null);
+
+            _act = action;
+            _secondDue = secondDue;
+        }
+
+        public void Execute(JobContext jobContext)
+        {
+            _act();
+        }
+
+        public TimeSpan GetNextInterval(JobContext jobContext)
+        {
+            return TimeSpan.MaxValue;
+        }
+
+        public TimeSpan Due
+        {
+            get { return TimeSpan.FromSeconds(_secondDue); }
+        }
+    }
+}
