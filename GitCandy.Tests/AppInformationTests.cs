92292332ad38eb8018702b3924b129d4836e4661
Aimeast <lixd3389@gmail.com>
2017-04-15 11:22:07 +0800

    Add xunit

---
+++ GitCandy.Tests/AppInformationTests.cs
@@ -0,0 +1,33 @@
+﻿using GitCandy.Base;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace GitCandy.Tests
+{
+    public class AppInformationTests
+    {
+        [Fact]
+        public void HasEmbeddedInformation()
+        {
+            Assert.Single(
+                typeof(AppInformation)
+                    .GetTypeInfo()
+                    .Assembly
+                    .GetManifestResourceNames(),
+                s => s.EndsWith(".Information"));
+        }
+
+        [Fact]
+        public void CopyrightUpdated()
+        {
+            Assert.Equal(
+                "Copyright © 2013-" + DateTime.Now.Year,
+                typeof(AppInformation)
+                    .GetTypeInfo()
+                    .Assembly
+                    .GetCustomAttribute<AssemblyCopyrightAttribute>()
+                    .Copyright);
+        }
+    }
+}
